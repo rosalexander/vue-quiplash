cf2a69490feafe2deeae4bed511882806f19c935
Alvin N <38022548+alvindn@users.noreply.github.com>
2018-11-19 12:51:23 -0800

    Updated index.js

--- src/router/index.js
+++ src/router/index.js
@@ -8,11 +8,7 @@ import Ready from '@/components/Ready'
 import Game from '@/components/Game'
 import Answers from '@/components/Answers'
 import Results from '@/components/Results'
-<<<<<<< HEAD
 import Response from '@/components/Response'
-=======
-import Timer from '@/components/Timer'
->>>>>>> 4ac5f77bd09d569b2c7b8fc9b6b42f5e23df997c
 
 Vue.use(Router)
 
@@ -63,15 +59,9 @@ const routes = [
   },
 
   {
-<<<<<<< HEAD
     path: '/response',
     name: 'Response',
     component: Response
-=======
-    path: '/timer',
-    name: 'Timer',
-    component: Timer
->>>>>>> 4ac5f77bd09d569b2c7b8fc9b6b42f5e23df997c
   }
 ]
 
