414a059fa7c673fc220da1bb1aa9776c66e18914
Alvin Nguyen <alvindn@uci.edu>
2018-12-06 14:38:44 -0800

    updates to body/format

--- src/components/Ready.vue
+++ src/components/Ready.vue
@@ -4,9 +4,9 @@
             GET READY
         </div>
         <div class="roundMessage">
-            Round 1 will begin in...
+            Round 1 is starting soon...
         </div>
-        <div id="countdown">
+        <!-- <div id="countdown">
             <div id='tiles'>
                 <span style = "text-align: center">
                     {{ seconds }}
@@ -15,31 +15,53 @@
             <div class="labels">
                 <li style="font-family: 'Krungthep'; font-size: 20px; padding: 3px;">Seconds</li>
             </div>
-        </div> 
+        </div>  -->
+        <div class="progress-bar">
+
+            <div class="progress" id="progress"></div>
+
+
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: 'Ready',
-    data: function(){
-        return {
-            seconds: '5',
-        }
+    mounted() {
+        this.progress()
     },
 
     methods: {
-        updateTimer: function() {
-            if (this.seconds != '0'){
-                this.seconds = (parseInt(this.seconds) - 1).toString();
+        // updateTimer: function() {
+        //     if (this.seconds != '0'){
+        //         this.seconds = (parseInt(this.seconds) - 1).toString();
+        //     }
+        // },
+
+        progress() {
+            var prg = document.getElementById('progress');
+            var counter = 5;
+            var progress = 25;
+            var id = setInterval(frame, 150);
+
+            function frame() {
+                if(progress == 500 && counter == 100) {
+                    clearInterval(id);
+                }
+                else {
+                    progress += 5;
+                    counter += 1;
+                    prg.style.width = progress + 'px';
+                }
             }
-        }
+        },
 
     },
 
-    created() {
-        setInterval(this.updateTimer, 1000)
-    }
+    // created() {
+    //     setInterval(this.updateTimer, 1000)
+    // }
 }
 </script>
 
