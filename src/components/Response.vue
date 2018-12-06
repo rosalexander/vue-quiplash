d8da18e96dda14702e071f1773cb4cb9fae031a7
Alexander Ros <alros@Alexanders-MacBook-Pro.local>
2018-12-06 13:08:43 -0800

    Adding functionality to Response

--- src/components/Response.vue
+++ src/components/Response.vue
@@ -9,20 +9,23 @@
         <div class="headerRound">
             ROUND 1
         </div>
-        <div class="question">
-            "A test prompt"
-        </div>
-        <hr>
-        <div class="cardRow">
-            <form action="">
-                <textarea rows="10" cols="30" maxlength= 140 id="response" placeholder="Enter response here..."></textarea>
-            </form>
+        <div v-if="!admin">
+            <div class="question">
+                "A test prompt"
+            </div>
+            <hr>
+            <div class="cardRow">
+                <form action="">
+                    <textarea rows="10" cols="30" maxlength= 140 id="response" placeholder="Enter response here..."></textarea>
+                </form>
 
+            </div>
+
+            <div>
+                <button class="btnPin">SUBMIT</button>
+            </div>
         </div>
 
-        <div>
-            <button class="btnPin">SUBMIT</button>
-        </div>
  </div>
 </template>
 
@@ -41,6 +44,7 @@
                 username: '',
                 members: [],
                 prompts: [],
+                admin: false,
                 pin: this.$route.params.id
             }
         },
@@ -57,10 +61,12 @@
                 if (data.user_id == this.user_id) {
                     this.username = data.username
                     this.addUser();
+                    if (this.username === 'Admin') {
+                        this.admin = true;
+                        this.socket.emit('set_prompts', this.pin)
+                    }
                 }
             }.bind(this))
-
-            this.socket.emit('set_prompts', this.pin)
         },
 
         mounted() {
